be2313b94aad2496c00e439fc95f17ae94855ee3
OpenShift guest <kgoldman2@mail.valenciacollege.edu>
2012-12-15 09:03:15 -0500

    final calculator dojo production

---
+++ final-calc-production.php
@@ -0,0 +1,36 @@
+<?php
+
+// production code for final calculator dojo
+
+class Calculator 
+{
+    public $register = 0;
+    
+// Sunday
+    // function for addition
+    function add($a, $b )
+    {
+       return $a+$b;
+    }
+
+// Monday
+    // function for subtraction
+    function sub( $a, $b)
+    {
+         return $a-$b;
+    }
+
+// Tuesday
+    // function for multiplication
+    function multi( $a, $b)
+    {
+       return $a*$b;
+    }
+
+// Wednesday
+    // function for division
+    function div( $a, $b)
+    {
+       return $a/$b;
+    }
+}
